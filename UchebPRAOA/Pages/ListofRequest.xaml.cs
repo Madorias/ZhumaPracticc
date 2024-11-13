36e17bd0d07a452c6e7eda657c08ce6787def7d1
Паршин Олег Андреевич <dawntrail@mail.ru>
2024-11-13 15:18:26 +0300

    araara

---
+++ UchebPRAOA/Pages/ListofRequest.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UchebPRAOA.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ListofRequest.xaml
+    /// </summary>
+    public partial class ListofRequest : Page
+    {
+        public ListofRequest()
+        {
+            InitializeComponent();
+            ListRequest.ItemsSource = UchebPracsEntities1.GetContext().Request.ToList();
+        }
+
+        private void AddRequest_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.frame.Navigate(new Pages.AddRequest());
+        }
+
+        private void EditRequest_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void EDitBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
